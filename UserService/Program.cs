8374dd4a62129be04fa31d1df03435fe8e6b1cc8
Kevin Romsom <392873@student.fontys.nl>
2023-06-09 16:00:06 +0200

    migrate db

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -25,6 +25,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<UserContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
